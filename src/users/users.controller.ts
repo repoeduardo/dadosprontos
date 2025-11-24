9e6297adbe2395cd76ed272b4b7b01bf13de06fe
Eduardo Araujo <eduardoaraujodasilva@protonmail.com>
2025-11-24 19:59:55 +0000

    feat: cria algumas rotas e parametros em /usuarios

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,9 +1,32 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
+import { UsersService } from './users.service';
 
 @Controller('usuarios')
 export class UsersController {
-  @Get('hello')
-  hello() {
-    return 'Rota de usuarios em funcionamento...';
+  constructor(private readonly usersService: UsersService) {}
+  @Get()
+  findAll(
+    @Query('limite') limite?: string,
+    @Query('pulo') pulo?: string,
+    @Query('selecione') selecione?: string,
+  ) {
+    const limit = limite ? parseInt(limite, 10) : 10;
+    const skip = pulo ? parseInt(pulo, 10) : 0;
+    const select = String(selecione);
+    return this.usersService.findAll(limit, skip, select);
+  }
+  @Get('aleatorios')
+  findRandom() {
+    return this.usersService.findRandom();
+  }
+
+  @Get('procurar')
+  search(@Query('q') query: string) {
+    return this.usersService.search(query);
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.usersService.findOne(Number(id));
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,9 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+  Query,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { Repository } from 'typeorm';
@@ -9,8 +14,128 @@ export class UsersService {
     @InjectRepository(User) private readonly userRepository: Repository<User>,
   ) {}
 
-  async findAll() {
-    const users = await this.userRepository.find();
+  async findAll(limit: number = 10, skip: number = 0, select?: string) {
+    if (limit < 0 || skip < 0) {
+      throw new BadRequestException(
+        'Limite e pulo devem ser números positivos',
+      );
+    }
+
+    // Limite máximo - questões de segurança
+    const MAX_LIMIT = 50;
+
+    const queryBuilder = this.userRepository.createQueryBuilder('user');
+
+    // Se passar select, aplica os campos específicos
+    if (select) {
+      const fields = select.split(',').map((field) => field.trim());
+
+      // Pega os campos válidos do metadata da entidade
+      const metadata = this.userRepository.metadata;
+      const validFields = metadata.columns.map((col) => col.propertyName);
+
+      // Valida campos
+      const invalidFields = fields.filter(
+        (field) => !validFields.includes(field),
+      );
+      if (invalidFields.length > 0) {
+        throw new BadRequestException(
+          `Campos inválidos: ${invalidFields.join(', ')}`,
+        );
+      }
+
+      /* Campos sensíveis que não podem ser selecionados -> verificar depois se faz sentido ativar esse:
+      const camposSensiveis = ['senha'];
+      const camposProibidos = campos.filter((campo) =>
+        camposSensiveis.includes(campo),
+      );
+
+      if (camposProibidos.length > 0) {
+        throw new BadRequestException(
+          `Não é permitido selecionar campos sensíveis: ${camposProibidos.join(', ')}`,
+        );
+      }
+        */
+
+      // Sempre incluir o ID
+      if (!fields.includes('id')) {
+        fields.unshift('id');
+      }
+
+      queryBuilder.select(fields.map((field) => `user.${field}`));
+    }
+
+    // Retorna todos se limite = 0
+    if (limit === 0) {
+      const users = await queryBuilder.getMany();
+      if (!users) {
+        throw new NotFoundException();
+      }
+
+      return {
+        usuarios: users,
+        total: users.length,
+        limite: 0,
+        pulo: 0,
+      };
+    }
+
+    const secureLimit = Math.min(limit, MAX_LIMIT);
+    const total = await queryBuilder.getCount();
+    queryBuilder.skip(skip).take(secureLimit);
+    const users = await queryBuilder.getMany();
+
+    return {
+      usuarios: users,
+      total_de_registros: total,
+      limite: secureLimit,
+      pulo: skip,
+      paginas: Math.ceil(total / secureLimit),
+      pagina_atual: Math.floor(skip / secureLimit) + 1,
+    };
+  }
+
+  async findOne(id: number) {
+    const user = await this.userRepository.findOne({
+      where: {
+        id: id,
+      },
+    });
+
+    if (!user) {
+      throw new NotFoundException();
+    }
+    return user;
+  }
+
+  async findRandom(limit: number = 5) {
+    const users = await this.userRepository
+      .createQueryBuilder('user')
+      .orderBy('RANDOM()')
+      .limit(limit)
+      .getMany();
+    if (!users) {
+      throw new NotFoundException();
+    }
+    return users;
+  }
+
+  async search(query: string, limit: number = 3) {
+    if (!query || query.trim() === '') {
+      throw new BadRequestException('Query de busca não pode estar vazia');
+    }
+    const users = await this.userRepository
+      .createQueryBuilder('user')
+      .where('LOWER(user.nome) LIKE LOWER(:query)', { query: `%${query}%` })
+      .orWhere('LOWER(user.sobrenome) LIKE LOWER(:query)', {
+        query: `%${query}%`,
+      })
+      .orWhere('LOWER(user.nome_usuario) LIKE LOWER(:query)', {
+        query: `%${query}%`,
+      })
+      .limit(limit)
+      .getMany();
+
     if (!users) {
       throw new NotFoundException();
     }
